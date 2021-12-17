d00351e5f8a85c54ba6751f99163869cf97f5b02
high-g <altema1217bbfumi@live.jp>
2021-12-17 12:04:54 +0900

    feat: tailwind

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,6 +2,7 @@ function App() {
   return (
     <div>
       <p className="text-3xl font-bold underline">test</p>
+      <p className="mt-[40px] md:mt-[20px]">margin-top: 40px</p>
     </div>
   )
 }
